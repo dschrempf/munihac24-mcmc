39c03793fa0a4a39e7094fd0224817d187645909
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-10-03 18:12:54 +0200

    proper gitignore and stub main

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      :  Main
+-- Description :  Estimate climate change using MCMC
+-- Copyright   :  2024 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Thu Oct  3 18:01:47 2024.
+module Main
+  ( main,
+  )
+where
+
+main :: IO ()
+main = undefined
